d16ce748785ff69e23e7b834c0404bfb2ee90eb1
Muhammad Musa <moosasaadat99@gmail.com>
2019-10-12 23:44:02 +0500

    Added Question Layout

--- src/Components/Questions.js
+++ src/Components/Questions.js
@@ -1,9 +1,20 @@
-import React, { useState } from "react";
+import React from "react";
+import { Link } from "react-router-dom";
+
+import "./Styles/Questions.css";
 
 function Questions () {
+	const answers = [ "A", "B", "C", "D" ];
+	const answerItems = answers.map((answer) => (
+		<li className="answers-item">
+			<Link className="answers-link">answer</Link>
+		</li>
+	));
+
 	return (
 		<div className="questions-wrapper">
-			<h1 className="questions-heading">Question 1</h1>
+			<h1 className="question">Question 1</h1>
+			<ul className="answers-list">{answerItems}</ul>
 		</div>
 	);
 }
