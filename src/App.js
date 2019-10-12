6197c20ede476acb54598f01bddf77d6b0c71be3
Muhammad Musa <moosasaadat99@gmail.com>
2019-10-12 21:49:44 +0500

    Added Login and Signup Form

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import { HashRouter, Route, Switch } from "react-router-dom";
 
 import Navbar from "./Components/Navbar";
+import SignUpForm from "./Components/SignUpForm";
+import LogInForm from "./Components/LogInForm";
 import "./App.css";
 
 function App () {
+	const [ hasAccount, setHasAccount ] = useState(true);
+
+	const formComponent = !hasAccount ? LogInForm : SignUpForm;
 	return (
 		<HashRouter>
 			<Navbar />
 			<div className="App">
 				<Switch>
-					<Route exact path="/" />
-					<Route exact path="/" />
+					<Route exact path="/" component={formComponent} />
 					<Route exact path="/" />
 				</Switch>
 			</div>
---
+++ src/Components/LogInForm.js
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+
+import "./Styles/Form.css";
+
+function LogInForm () {
+	const [ email, setEmail ] = useState("");
+	const [ password, setPassword ] = useState("");
+
+	function submitForm (e) {
+		e.preventDefault();
+	}
+
+	return (
+		<div className="form-wrapper">
+			<h1 className="form-heading">Log In</h1>
+			<form onSubmit={submitForm} className="form login">
+				<div className="input-field">
+					<i className="fas fa-envelope" />
+					<input
+						type="email"
+						placeholder="Email"
+						value={email}
+						onChange={(event) => setEmail(event.target.value)}
+					/>
+				</div>
+				<div className="input-field">
+					<i className="fas fa-key" />
+					<input
+						type="password"
+						placeholder="Password"
+						value={password}
+						onChange={(event) => setPassword(event.target.value)}
+					/>
+				</div>
+				<button className="form-btn" type="submit">
+					Log In
+				</button>
+			</form>
+		</div>
+	);
+}
+
+export default LogInForm;
