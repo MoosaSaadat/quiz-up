78618f11db5754c10a91516500c888c204f61cd6
Muhammad Musa <moosasaadat99@gmail.com>
2019-10-15 09:15:47 +0500

    Completed Game Component Layout and Functionality

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { HashRouter, Route, Switch } from "react-router-dom";
 
 import Navbar from "./Components/Navbar";
@@ -12,21 +12,36 @@ import * as ROUTES from "./Constants/Routes";
 import { withAuthentication } from "./Components/Session";
 import "./App.css";
 
-function App () {
+function App (props) {
+	const [ allData, setAllData ] = useState(null);
+
+	useEffect(() => {
+		props.firebase.db
+			.collection("categories")
+			.get()
+			.then(function (querySnapshot) {
+				var newList = [];
+				querySnapshot.forEach(function (doc) {
+					// console.log(doc.id, " => ", doc.data());
+					newList.push({ key: doc.id, ...doc.data() });
+				});
+				setAllData(newList);
+				console.log(newList);
+			})
+			.catch(function (error) {
+				console.log("Error getting documents: ", error);
+			});
+	}, []);
+
 	return (
 		<HashRouter>
 			<Navbar />
 			<div className="App">
 				<Switch>
-					{/* USER ROUTES */}
 					<Route exact path={ROUTES.HOME} component={CategoryList} />
 					<Route exact path={ROUTES.SIGN_UP} component={SignUpForm} />
 					<Route exact path={ROUTES.LOG_IN} component={LogInForm} />
 					<Route exact path={ROUTES.GAME} component={Game} />
-					{/* PORTAL ROUTES */}
-					{/* <Route exact path={ROUTES.PORTAL_HOME} component={CategoryList} />
-					<Route exact path={ROUTES.PORTAL_QUESTION} component={Questions} />
-					<Route exact path={ROUTES.PORTAL_USERS} component={Users} /> */}
 					<Route path={ROUTES.HOME} component={CategoryList} />
 				</Switch>
 			</div>
--- src/Components/Game.js
+++ src/Components/Game.js
@@ -1,19 +1,87 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import { withAuthorization } from "./Session";
+import * as ROUTES from "../Constants/Routes";
 import "./Styles/Game.css";
 
 function Game (props) {
-	return (
+	const [ questions ] = useState([
+		{
+			answers: [ "Hello", "How are you?", "Bye", "See you Soon" ],
+			correctAns: "0",
+			ques: "How to great someone?"
+		},
+		{
+			answers: [ "Hello", "How are you?", "Bye", "See you Soon" ],
+			correctAns: "0",
+			ques: "Question number 2?"
+		},
+		{
+			answers: [ "Hello", "How are you?", "Bye", "See you Soon" ],
+			correctAns: "0",
+			ques: "Question number 3?"
+		},
+		{
+			answers: [ "Hello", "How are you?", "Bye", "See you Soon" ],
+			correctAns: "0",
+			ques: "Question number 4?"
+		},
+		{
+			answers: [ "Hello", "How are you?", "Bye", "See you Soon" ],
+			correctAns: "0",
+			ques: "Question number 5?"
+		}
+	]);
+	const [ time, setTime ] = useState(10);
+	const [ score, setScore ] = useState(0);
+	const [ currQues, setCurrQues ] = useState(0);
+
+	useEffect(() => {
+		var myTimer = setInterval(() => {
+			setTime((old) => old - 1);
+		}, 1000);
+
+		return () => clearInterval(myTimer);
+	}, []);
+
+	useEffect(
+		() => {
+			if (time <= -3) props.history.push(ROUTES.HOME);
+		},
+		[ time ]
+	);
+
+	const handleClick = (e) => {
+		if (e.target.name === questions[currQues].correctAns) {
+			setScore((oldScore) => oldScore + 10);
+		}
+		changeQuestion();
+	};
+
+	const changeQuestion = () => {
+		if (currQues === questions.length - 1) setCurrQues(0);
+		else setCurrQues((old) => old + 1);
+	};
+
+	const answersList = questions[currQues].answers.map((ans, idx) => (
+		<button onClick={handleClick} key={idx} name={idx} className="answer">
+			{ans}
+		</button>
+	));
+
+	return time > 0 ? (
 		<div className="game-container">
-			<div className="time">60s</div>
-			<div className="question">What is your Name?</div>
-			<div className="answer-container">
-				<button className="answer">1. N</button>
-				<button className="answer">2. M</button>
-				<button className="answer">3. F</button>
-				<button className="answer">4. A</button>
+			<div className="info-container">
+				<h2 className="time">Remaining Time: {time}s</h2>
+				<h2 className="score">Score: {score}</h2>
 			</div>
+			<div className="question">{questions[currQues].ques}</div>
+			<div className="answer-container">{answersList}</div>
+		</div>
+	) : (
+		<div className="game-container">
+			<h2 className="score">Score: {score}</h2>
+			<div className="question">GAME OVER!</div>
 		</div>
 	);
 }
