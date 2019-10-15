63596d3d50314f877c536a5f233f6819297126c7
Muhammad Musa <moosasaadat99@gmail.com>
2019-10-15 21:31:58 +0500

    Updates

---
+++ precache-manifest.6974de6f16595941e94d7850d88bb041.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0328437463fdecf88501b511b53d39d1",
+    "url": "/quiz-up/index.html"
+  },
+  {
+    "revision": "3364875ebe6b1e915691",
+    "url": "/quiz-up/static/css/main.6a731c5d.chunk.css"
+  },
+  {
+    "revision": "3b5c6b160bc1eef34e51",
+    "url": "/quiz-up/static/js/2.33c6b2ae.chunk.js"
+  },
+  {
+    "revision": "3364875ebe6b1e915691",
+    "url": "/quiz-up/static/js/main.b0fb0c48.chunk.js"
+  },
+  {
+    "revision": "55be003249462975b279",
+    "url": "/quiz-up/static/js/runtime-main.7a2f3318.js"
+  }
+]);
